bbcaad0f3e4e7ca1291638a322ce4cab3390bd2d
Hiroshi Kunieda <hkwaver@gmail.com>
2018-11-11 18:26:53 +0900

    ダイスの追加

--- src/world/world_layer.lua
+++ src/world/world_layer.lua
@@ -3,7 +3,7 @@
 -- WorldLayer
 
 ------------------------------------------
-Global.WorldLayer = class("WorldLayer", cc.Node, Observable)
+Global.WorldLayer = class("WorldLayer", cc.Layer, Observable)
 
 local MyClass = Global.WorldLayer
 
@@ -25,7 +25,21 @@ local MyClass = Global.WorldLayer
     ------------------------------------------
     function MyClass:onEnter()
 
+        local dice = cc.Sprite3D:create("res/model/dice/Dice.c3b")
+        dice:setTexture("res/model/dice/d6-white-dots.png")
+        dice:setScale(20)
+        dice:setPosition(cc.p(300, 200))
+        self:addChild(dice)
+
+        local dice6 = dice:getMeshByName("d6")
+        local dice6Low = dice:getMeshByName("d6Low")
+        local dice10 = dice:getMeshByName("d10")
+        local dice10Low = dice:getMeshByName("d10Low")
+        dice6Low:setVisible(false)
+        dice10:setVisible(false)
+        dice10Low:setVisible(false)
     end
+
     ------------------------------------------
     -- onExit
     ------------------------------------------
