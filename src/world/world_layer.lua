179841bb6b6030cf91f726bd07c44942f3e48899
Hiroshi Kunieda <hkwaver@gmail.com>
2018-11-17 14:15:53 +0900

    ダイスを回転させる

--- src/world/world_layer.lua
+++ src/world/world_layer.lua
@@ -25,6 +25,23 @@ local MyClass = Global.WorldLayer
     ------------------------------------------
     function MyClass:onEnter()
 
+        self:initializeView()
+
+    end
+
+    ------------------------------------------
+    -- onExit
+    ------------------------------------------
+    function MyClass:onExit()
+
+    end
+
+
+    ------------------------------------------
+    -- initializeView
+    ------------------------------------------
+    function MyClass:initializeView()
+
         local dice = cc.Sprite3D:create("res/model/dice/Dice.c3b")
         dice:setTexture("res/model/dice/d6-white-dots.png")
         dice:setScale(20)
@@ -38,13 +55,46 @@ local MyClass = Global.WorldLayer
         dice6Low:setVisible(false)
         dice10:setVisible(false)
         dice10Low:setVisible(false)
+
+        local touchPanel = ccui.Layout:create()
+        touchPanel:setContentSize(display.width, display.height)
+        self:addChild(touchPanel)
+
+        local listener = cc.EventListenerTouchOneByOne:create()
+        listener:registerScriptHandler(handler(self, self.onTouchBegan), cc.Handler.EVENT_TOUCH_BEGAN )
+        listener:registerScriptHandler(handler(self, self.onTouchMoved), cc.Handler.EVENT_TOUCH_MOVED )
+        listener:registerScriptHandler(handler(self, self.onTouchEnded), cc.Handler.EVENT_TOUCH_ENDED )
+
+        local eventDispatcher = touchPanel:getEventDispatcher()
+        eventDispatcher:addEventListenerWithSceneGraphPriority(listener, touchPanel)
+
+        self._viewRef = {
+            dice = dice,
+        }
     end
 
     ------------------------------------------
-    -- onExit
+    -- onTouchBegan
     ------------------------------------------
-    function MyClass:onExit()
+    function MyClass:onTouchBegan(touch, event)
 
+        return true
+    end
+
+    ------------------------------------------
+    -- onTouchMoved
+    ------------------------------------------
+    function MyClass:onTouchMoved(touch, event)
+
+    end
+
+    ------------------------------------------
+    -- onTouchEnded
+    ------------------------------------------
+    function MyClass:onTouchEnded(touch, event)
+
+        local action = cc.RotateBy:create(1, cc.vec3(0, 90, 0))
+        self._viewRef.dice:runAction(action)
     end
 
 return MyClass
